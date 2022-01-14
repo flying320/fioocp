a494ac9053a14487aa1ba0a94aeec07316a7f8b0
Daniel Danner <ddanner@Daniels-MBP-16.fritz.box>
2022-01-14 11:46:17 +0100

    adding ssh to push fio files to container from ansible

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,26 @@
 FROM alpine
-RUN apk add fio
-WORKDIR /fioconf
 
+# add openssh and fio
+RUN apk add fio openssh openrc bash sudo
+
+
+#Enable the sshd service so that it starts at boot: 
+RUN rc-update add sshd default
+
+#Enable public key authentication
+RUN echo 'PubkeyAuthentication yes' >> /etc/ssh/sshd_config
+
+
+#Add user 
+RUN adduser -h /home/ddanner -s /bin/sh -D ddanner
+RUN mkdir -m 700 /home/ddanner/.ssh  
+
+# Copy public key
+COPY id_rsa.pub /home/ddanner/.ssh/authoriyed_keys
+
+
+EXPOSE 22
+
+CMD ["/etc/init.d/sshd start"]
+
+WORKDIR /fioconf
--- runfio.sh
+++ runfio.sh
@@ -9,20 +9,23 @@ mkdir -p /fioconf/results
 while true
 do
         cd /fioconf/jobs/todo
-        for file in `ls  2>/dev/null`
+        for file in `ls |head -1  2>/dev/null`
         do
                 DAT=`date +%y%m%d_%H%M%S`
-                echo "Progressing file $file"
+                TESTFILE=/fiodata/testfile_${DAT}
+
+                echo "Progressing file $file with testfile $TESTFILE and output /fioconf/results/${file}.${DAT}.json"
                 cd /fioconf/jobs/todo
                 mv $file /fioconf/jobs/inprogress
                 cd /fioconf/jobs/inprogress
 
-                /usr/bin/fio --output=/fioconf/results/${file}.${DAT}.json --output-format=json  $file
+                /usr/bin/fio --output=/fioconf/results/${file}.${DAT}.json --output-format=json --size=10G --filename=${TESTFILE}  $file
                 sleep 60
 
                 mv $file /fioconf/jobs/done
+                rm ${TESTFILE}
         done
 
-        echo "No fio job file found"
+        echo "No fio job file found - sleeping "
         sleep 60
 done
